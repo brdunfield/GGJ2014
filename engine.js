00238a18e7ee95bda1419d3667c7d11c691de42f
Bethany <brdunfield@gmail.com>
2014-01-24 21:07:56 -0500

    Making everything time based -i.e. speed is now 200px / sec
    - code organization

--- engine.js
+++ engine.js
@@ -6,18 +6,26 @@ var Engine = function(canvasID) {
     this.context = canvas.getContext('2d');
     
     this.startTime = 0;
+    this.lastTime = 0;
     
     // variables
-    this.speed = 1;
+    this.speed = 500; // px/s
+    this.gravity = 50; //px/s
     // this.difficulty
     // this.points;
     // this.colour;
     this.char = new Character();
     
     this.worldCoords = [[0, getHeight()/2], [200, getHeight()/2]];
-    this.platformCoords = [];
+    this.platformCoords = [];  
     this.tx = 0;
     this.ty = 0;
+    
+    window.addEventListener('keydown', function(e) {
+        if (e.keyCode == 32) {
+            self.char.jump();
+        }
+    });
 };
 
 Engine.prototype.start = function() {
@@ -32,11 +40,11 @@ Engine.prototype.start = function() {
 Engine.prototype.animate = function(time) {
     var self = this,
         context = this.context;
-    
+    var timeSinceLastFrame = time - this.lastTime;
     
     // Update
-    
     this.updateWorld();
+    this.translateWorld(timeSinceLastFrame);
     
     
     // Draw
@@ -59,34 +67,33 @@ Engine.prototype.animate = function(time) {
     
     
     // call next frame
+    this.lastTime = time;
     window.requestAnimationFrame(function(time) {
         self.animate.call(self, time);
     });
 };
 
-Engine.prototype.updateWorld = function() {
+Engine.prototype.translateWorld = function(t) {
     var d = new Date();
-    this.speed = 1 + (d.getTime() - this.startTime)/10000;
+    this.speed = this.speed + t/1000;
 
     // Move world according to speed
     for (var i = 0; i < this.worldCoords.length; i++) {
-        this.worldCoords[i][0] -= this.speed;
-        if (this.char.falling) {
-            var fallV = (d.getTime() - this.char.falling)/1000 * 200;
-            this.worldCoords[i][1] -= fallV;
+        // x
+        this.worldCoords[i][0] -= this.speed*t/1000;
+        // y
+        if (this.char.isJumping) {
+            this.char.jumpV = this.char.jumpV - (t*this.gravity/1000);
+            this.worldCoords[i][1] += this.char.jumpV;
         }
-        if (this.char.climbBy) {
+        else if (this.char.falling) {
+            this.char.jumpV = this.char.jumpV - (t*this.gravity/1000);
+            this.worldCoords[i][1] += this.char.jumpV;
+        }
+        else if (this.char.climbBy) {
             this.worldCoords[i][1] -= this.char.climbBy;
         }
     }
-    // get rid of old coords
-    if (this.worldCoords[1][0] < 0) {
-        this.worldCoords.splice(0,1);
-    }
-    // add a new coord if needed
-    var lastCoord = this.worldCoords[this.worldCoords.length - 1];
-    if (lastCoord[0] < getWidth())
-        this.worldCoords.push([lastCoord[0] + Math.random()*300 + 200, lastCoord[1] - (Math.random()*200 - 100)]);
     
     // gravity - fall until you hit a line
     // determine two points around the char to determine the line segment
@@ -99,21 +106,36 @@ Engine.prototype.updateWorld = function() {
             break;
         }
     }
+    
+    // determine how far away the char is from the line, and whether falling or climbing
     var slope = (rightPt[1] - leftPt[1]) / (rightPt[0] - leftPt[0]);
     var dx = this.char.x - leftPt[0];
     //console.log("slope: " + slope);
-    if ((slope*dx + leftPt[1]) > this.char.y) {
+    if ((slope*dx + leftPt[1]) > this.char.y && !this.char.jumping) {
         //console.log("Char needs to fall now");
         this.char.falling = d.getTime();
-        this.fallAcc = 1;
+        this.char.jumpV = 0;
         this.char.climbBy = null;
     } else if((slope*dx + leftPt[1]) < this.char.y) {
         this.char.climbBy = (slope*dx + leftPt[1]) - this.char.y;
         this.char.falling = null;
+        this.char.isJumping = null;
     } else {
         this.char.falling = false;
         this.char.climbBy = null;
+        this.char.isJumping = false;
     }
+};
+
+Engine.prototype.updateWorld = function() {
+    // get rid of old coords
+    if (this.worldCoords[1][0] < 0) {
+        this.worldCoords.splice(0,1);
+    }
+    // add a new world chunk if needed
+    var lastCoord = this.worldCoords[this.worldCoords.length - 1];
+    if (lastCoord[0] < getWidth())
+        this.worldCoords.push([lastCoord[0] + Math.random()*300 + 200, lastCoord[1] - (Math.random()*200 - 100)]);    
 };
 
 /* ~~ Helper Functions */
