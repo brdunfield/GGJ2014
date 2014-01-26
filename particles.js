747f2b83604a9959e4738842d12d6fc46990fa3e
Andrew Brough <spectrum1012@gmail.com>
2014-01-25 21:44:03 -0500

    Readded projectiles and shields

--- particles.js
+++ particles.js
@@ -1,10 +1,11 @@
 //emitter to place at a location
-var particleEmitter = function(context, position, velocity, particleVelocity, colour, size, rate, maxParticles){
+var particleEmitter = function(context, type, position, velocity, particleVelocity, hue, size, rate, maxParticles, life){
     this.context = context;
+    this.type = type;
     this.position = position;
     this.velocity = velocity;
     this.particleVelocity = particleVelocity;
-    this.colour = colour;
+    this.hue = hue;
     this.rate = rate;
     this.size = size;
     this.maxParticles = maxParticles;
@@ -13,7 +14,7 @@ var particleEmitter = function(context, position, velocity, particleVelocity, co
 }
 
 particleEmitter.prototype.makeParticle = function(){
-    this.particles.push(new particleSquare(this.context, this.position, this.particleVelocity, this.colour, this.size));
+    this.particles.push(new particle(this.context, this.type, this.position, this.particleVelocity, this.hue, this.size));
 }
 
 particleEmitter.prototype.run = function(time){
@@ -31,38 +32,64 @@ particleEmitter.prototype.run = function(time){
     
     
     //draw the projectile
-    this.context.fillRect(this.position[0], this.position[1], 10,5);
+    if(this.type == "rect"){
+        this.context.fillStyle = "hsla(" + this.hue + ", 90%, 70%, 0.9)";
+        this.context.strokeStyle = "hsla(" + this.hue + ", 90%, 90%, 0.9)";
+        this.context.lineWidth = 2;
+        this.context.fillRect(this.position[0], this.position[1], 20,10);
+    }
 }
 
 
 
 //individual particle
-var particleSquare = function(context, origin, velocity, colour, size){
+var particle = function(context, type, origin, velocity, hue, size){
     this.context = context;
+    this.type = type;
     this.position = new Array();
     this.position[0] = origin[0];
     this.position[1] = origin[1];
-    this.rotation = new Array();
-    this.rotation[0] = Math.random();
-    this.rotation[1] = Math.random();
+    this.rotation = 0;
     this.velocity = new Array();
     this.velocity[0] = Math.floor((Math.random()*velocity[0])-velocity[0]/2);
     this.velocity[1] = Math.floor((Math.random()*velocity[1])-velocity[1]/2);
-    this.colour = colour;
+    this.hue = hue;
+    this.opacity = 1.0;
     this.size = size;
 }
 
-particleSquare.prototype.update = function(){
+particle.prototype.update = function(){
     this.position[0] += this.velocity[0];
     this.position[1] += this.velocity[1];
     this.rotation[0] += 0.1;
     this.rotation[1] += 0.1;
+    
+    this.opacity -= 0.1;
 }
-particleSquare.prototype.draw = function(){
-    this.context.fillStyle = "rgb(" + Math.round(this.colour[0]) + ", " + Math.round(this.colour[1]) + ", " + Math.round(this.colour[2]) + ")";
-    this.context.fillRect(this.position[0], this.position[1], this.size, this.size);
+particle.prototype.drawRect = function(){
+    this.context.fillStyle = "hsla(" + this.hue + ", 90%, 40%, " + this.opacity + ")";
+    this.context.strokeStyle = "hsla(" + this.hue + ", 90%, 90%, " + this.opacity + ")";
+    this.context.lineWidth = 2;
+    this.context.fillRect(this.position[0], this.position[1], this.size[0], this.size[1]);
 }
-particleSquare.prototype.run = function(){
+particle.prototype.drawEllipse = function(){
+    this.context.beginPath();
+    this.context.fillStyle = "hsla(" + this.hue + ", 90%, 40%, " + 0 + ")";
+//    this.context.fillStyle = "hsla(" + this.hue + ", 90%, 40%, " + this.opacity + ")";
+    this.context.strokeStyle = "hsla(" + this.hue + ", 90%, 60%, " + this.opacity + ")";
+    this.context.lineWidth = 2;
+//    this.context.arc(this.position[0], this.position[1] - this.size[0], this.size[1], 0, Math.twoPI, false);
+    this.context.ellipse(this.position[0], this.position[1], this.size[0], this.size[1], this.rotation, 0, 2*Math.PI, false);
+//    this.context.fill();
+    this.context.stroke();
+    this.context.closePath();
+}
+
+particle.prototype.run = function(){
     this.update();
-    this.draw();
+    //pick a rect or an ellipse
+    if(this.type=="ellipse") this.drawEllipse();
+    else if(this.type=="rect") this.drawRect();
 }
+
+
